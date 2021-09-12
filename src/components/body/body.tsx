25d54338e5311a552efdc0f18f56616c635ca6cd
eme92 <angel92-eme@hotmail.com>
2021-09-12 15:19:50 -0500

    Header Agregato

---
+++ src/components/body/body.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './body.css';
+
+export default function Body(){
+    return (
+        <div className='container__body'>
+            Body Insertado Correctamente
+        </div>
+    );
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.css';
+
+export default function Header(){
+    return (
+        <div className='container__header'>
+            Header Inserted rightly
+        </div>
+    );
+}
