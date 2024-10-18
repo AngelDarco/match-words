21f63d2b052b484f011c12210be704f2e1e40a97
angeldarco <angel92-eme@hotmail.com>
2024-10-18 04:33:28 -0500

    :package: tailwindcss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Project init ...</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-red-600 text-center">
+      Project init ...
+    </h1>
+  );
 }
 
 export default App;
