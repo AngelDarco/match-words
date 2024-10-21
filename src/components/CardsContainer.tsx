89ae6f6f9d4210b031183cf4f9939aa48e868ce7
angeldarco <angel92-eme@hotmail.com>
2024-10-21 07:33:03 -0500

    :hammer: words select in the same container update his same state

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,19 +1,36 @@
+import { RootState } from "../redux/store";
 import Card from "./Card";
+import { useDispatch, useSelector } from "react-redux";
+import { word, translation } from "../redux/slices/matchs";
+import { useEffect } from "react";
 
 export default function CardsContainer() {
   const words1 = ["Hello", "Bye", "Thanks"];
   const words2 = ["Hallo", "Tschus", "Danke"];
 
+  const state = useSelector((state: RootState) => state.match);
+  const dispatch = useDispatch();
+
   const handlerMatchs = (event: React.MouseEvent<HTMLElement>) => {
-    if (event.target === event.currentTarget) return;
-    const words = (event.target as HTMLElement).innerText;
-    console.log(words);
+    const target = event.target as HTMLElement;
+    if (target === event.currentTarget) return;
+    const id = event.currentTarget.id;
+    const words = target.innerText;
+
+    if (id === "lang") {
+      dispatch(word(words));
+    } else dispatch(translation(words));
   };
+
+  useEffect(() => {
+    console.log("rendering ...", state);
+  }, [state]);
 
   return (
     <div className="max-w-lg border h-[500px] flex items-center justify-center">
       <section
         className="w-full h-full flex flex-col items-center justify-center gap-2 hover:[&>div]:bg-slate-800 "
+        id="lang"
         onClick={handlerMatchs}
       >
         {words1.map((w, i) => (
@@ -23,6 +40,7 @@ export default function CardsContainer() {
       <section
         className="w-full h-full flex flex-col items-center justify-center gap-2 hover:[&>div]:bg-slate-800"
         onClick={handlerMatchs}
+        id="targ"
       >
         {words2.map((w, i) => (
           <Card key={i} card={w} match={undefined} />
