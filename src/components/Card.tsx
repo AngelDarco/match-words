42521cca2bf5e97c45ac7ccc012a16409d1afd0a
angeldarco <angel92-eme@hotmail.com>
2024-10-23 02:08:23 -0500

    :sparkles: styles added to the selected words

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,22 @@
 interface Card extends React.PropsWithChildren {
   card: string;
-  match?: boolean;
+  match: boolean | undefined;
 }
 export default function Card({ card, match }: Card) {
+  const HandlerMatchs = (event: React.MouseEvent<HTMLElement>) => {
+    if (match === undefined) {
+      const target = event.target as HTMLElement;
+      target.classList.toggle("bg-green-500");
+    }
+  };
   return (
     <div
-      className={`max-w-36 min-w-36 border rounded-lg p-2 flex align-center justify-center ${
-        match ? "bg-green-800" : match === false ? "bg-red-800" : ""
-      }`}
+      className={`max-w-36 min-w-36 border rounded-lg p-2 flex align-center justify-center
+      ${match ? "bg-green-500" : match === false ? "bg-red-500" : ""}
+      `}
+      onClick={HandlerMatchs}
     >
-      {card}{" "}
+      {card}
     </div>
   );
 }
