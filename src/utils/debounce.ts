d30ea446b13ac28e23eeb068a8df775c3fbe8a51
Angel Darco <angel92-eme@hotmail.com>
2025-02-03 12:39:42 -0500

    :fire: utils directory renamed to lib

--- src/utils/debounce.ts
+++
@@ -1,9 +0,0 @@
-export function debounce<M extends (...args: unknown[]) => unknown>(
-  callback: (...args: Parameters<M>) => void
-) {
-  let id: ReturnType<typeof setTimeout>;
-  return function (...args: Parameters<M>) {
-    clearTimeout(id);
-    id = setTimeout(() => callback(...args), 2000);
-  };
-}
