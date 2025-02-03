d8e2055b47d9a31c27066cc6027cee1a018915ee
Angel Darco <angel92-eme@hotmail.com>
2025-02-03 12:42:31 -0500

    :racehorse: new types added

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,18 @@
 export type Lang = "en" | "de";
 
+// data retrieved from the database
 export interface Data {
   lang: Lang;
   words: Array<{
     union_id: string;
     word: string;
   }>;
+}
+
+// card props
+interface Card extends React.PropsWithChildren {
+  word: string;
+  match: boolean;
+  isSelected: boolean;
+  id: string;
 }
