80513954db9a9c0cadf8bc28e1584353b723d398
Angel Darco <angel92-eme@hotmail.com>
2025-02-03 12:43:38 -0500

    :hammer: redux implemented to handle match states

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,85 +1,127 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
-import { word, translation, match, score } from "../redux/slices/matchs";
-import { useEffect } from "react";
+import { current, target, match, score } from "../redux/slices/matchs";
+import { useEffect, useState } from "react";
 import Card from "./Card";
+import getData from "../lib/getData";
+import { Data } from "../types";
+import Debounce from "../lib/Debounce";
 
 export default function CardsContainer() {
-  const words1 = ["Hello", "Bye", "Thanks"];
-  const words2 = ["Hallo", "Tschus", "Hello"];
-
+  const [data, setData] = useState<Data[]>([]);
   const dispatch = useDispatch();
-  const state = useSelector((state: RootState) => state.match);
+  const state = useSelector((state: RootState) => state.matchWords);
+
+  // get the data from the database
+  useEffect(() => {
+    (async () => {
+      const URL = "database";
+      const data = new getData(URL);
+
+      const curr = await data.getCurrentLanguage("en");
+      const targ = await data.getTargetLanguage("de");
+
+      if (!curr || !targ) return;
+      setData([curr, targ]);
+    })();
+  }, []);
+
+  // reset match state after 1 second every time a match is found
+  useEffect(() => {
+    const resetMatch = new Debounce();
+    if (state.match !== undefined)
+      resetMatch.execute(() => dispatch(match(undefined)));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.match]);
+
+  // check if the words match with his traduced word
+  useEffect(() => {
+    if (state.current.word && state.target.word) {
+      const isMatch = state.current.id === state.target.id;
+
+      // Set the match state
+      dispatch(match(isMatch));
+
+      // Increment the score
+      if (isMatch) dispatch(score());
+
+      // reset values
+      const resetTarget = new Debounce();
+      const resetCurrent = new Debounce();
+      resetTarget.execute(() => dispatch(target({ word: "", id: "" })));
+      resetCurrent.execute(() => dispatch(current({ word: "", id: "" })));
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.current.word, state.target.word]);
 
   const HandlerMatchs = (event: React.MouseEvent<HTMLElement>) => {
-    const target = event.target as HTMLElement;
-    if (target === event.currentTarget) return;
-    const id = event.currentTarget.id;
-    const words = target.innerText;
+    const targetElement = event.target as HTMLElement;
+    if (targetElement === event.currentTarget) return;
 
-    // select the original words
-    if (id === "lang") {
-      // add and remove the word from the state, adding a green background
-      if (words === state.word) dispatch(word(""));
-      else dispatch(word(words));
-    }
-    // select the traduced words
-    else {
-      // add and remove the word from the state, adding a green background
-      if (words === state.translation) dispatch(translation(""));
-      else dispatch(translation(words));
-    }
+    const { id, innerText } = targetElement;
+    const containerId = targetElement.parentElement?.id;
+
+    if (!id || !containerId) return;
+
+    const currentContainer = data[0]?.lang === containerId;
+    const targetContainer = data[1]?.lang === containerId;
+
+    // create or remove the current word state
+    if (currentContainer)
+      dispatch(
+        current(
+          innerText === state.current.word
+            ? { word: "", id: "" }
+            : { word: innerText, id }
+        )
+      );
+
+    // create or remove the target word state
+    if (targetContainer)
+      dispatch(
+        target(
+          innerText === state.target.word
+            ? { word: "", id: "" }
+            : { word: innerText, id }
+        )
+      );
   };
 
-  useEffect(() => {
-    // check if the words match with his traduced word
-    if (state.word && state.translation) {
-      if (state.word === state.translation) dispatch(match(true));
-      else dispatch(match(false));
-    }
-
-    // reset values
-    let id: NodeJS.Timer;
-    if (state.match !== undefined) {
-      id = setTimeout(() => {
-        dispatch(match(undefined));
-        dispatch(word(""));
-        dispatch(translation(""));
-
-        if (state.match) dispatch(score(state.score + 1));
-      }, 1000);
-    }
-
-    return () => clearTimeout(id);
-  }, [dispatch, state]);
+  const currentWords = data[0]?.words || [];
+  const targetWords = data[1]?.words || [];
 
   return (
     <div className="max-w-lg border h-[500px] flex items-center justify-center">
       <section
         className="w-full h-full flex flex-col items-center justify-center gap-2 "
-        id="lang"
+        id={data[0]?.lang}
         onClick={HandlerMatchs}
       >
-        {words1.map((w, i) => (
+        {currentWords.map(({ union_id, word }) => (
           <Card
-            key={i}
-            card={w}
-            state={state.word}
-            match={w === state.word ? state.match : undefined}
+            key={union_id}
+            id={union_id}
+            word={word}
+            isSelected={state.current.word === word}
+            match={state.current.word === word && state.match}
           />
         ))}
       </section>
+
       <section
         className="w-full h-full flex flex-col items-center justify-center gap-2 "
         onClick={HandlerMatchs}
-        id="targ"
+        id={data[1]?.lang}
       >
-        {words2.map((w, i) => (
+        {targetWords.map(({ union_id, word }) => (
           <Card
-            key={i}
-            card={w}
-            state={state.translation}
-            match={w === state.translation ? state.match : undefined}
+            key={union_id}
+            id={union_id}
+            word={word}
+            isSelected={state.target.word === word}
+            match={state.target.word === word && state.match}
           />
         ))}
       </section>
