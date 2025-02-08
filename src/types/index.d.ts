10fa84f6e83976dc870162d3aa134e1cc0e9dd1b
Angel Darco <angel92-eme@hotmail.com>
2025-02-08 12:20:57 -0500

    :racehorse: types updated and jsdoc comments added

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,17 +1,37 @@
+/**
+ * Language type: the abreviation of the language
+ * @param en english
+ * @param de german
+ */
 export type Lang = "en" | "de";
 
-export type Word = Array<{
-  union_id: string;
+/**
+ * Word interface
+ * @param union_id the union id of the word
+ * @param word the word value
+ */
+export type Word = {
+  union_id: number;
   word: string;
-}>;
+};
 
-// data retrieved from the database
+/**
+ * Data interface
+ * @param lang the current user language abreviation
+ * @param words an array of type Word {@link Word}
+ */
 export interface Data {
   lang: Lang;
-  words: Word;
+  words: Word[];
 }
 
-// card props
+/**
+ * Card interface
+ * @param word the word value
+ * @param match true if the word is a match, false otherwise
+ * @param isSelected true if the card is selected, false otherwise
+ * @param id the id of the card
+ */
 interface Card extends React.PropsWithChildren {
   word: string;
   match: boolean;
