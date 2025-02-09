8c0202307ab97ab97842eba28179eea018c06b4b
Angel Darco <angel92-eme@hotmail.com>
2025-02-09 11:33:00 -0500

    :lipstick: word cards styles updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,32 @@
 import { memo } from "react";
 import type { Card } from "../types";
 
-export default memo(function Card({ word, id, isSelected, match }: Card) {
+export default memo(function Card({
+  word,
+  id,
+  isSelected,
+  match,
+  isActive = undefined,
+}: Card) {
   const handlerBackground = () => {
+    if (isActive) return "";
     if (isSelected && match === false) {
       return "bg-red-500";
     } else if (isSelected) {
       return "bg-green-500";
     }
+    return "";
   };
 
   return (
     <div
-      className={`max-w-36 min-w-36 border rounded-lg p-2 flex align-center justify-center
-      ${handlerBackground()}`}
+      className={`max-w-36 min-w-36 border rounded-lg p-2 flex align-center justify-center ease-in-out delay-200 cursor-pointer ${
+        isActive
+          ? "opacity-50 cursor-default"
+          : isSelected
+          ? ""
+          : "hover:bg-gray-500/20"
+      } ${handlerBackground()}`}
       id={id}
     >
       {word}
